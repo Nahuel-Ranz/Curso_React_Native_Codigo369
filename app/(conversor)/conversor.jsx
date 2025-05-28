769b52bd2b07386d7262142491cde990d301e91f
Nahuel-Ranz <elhacha_nahuel@hotmail.com>
2025-05-28 18:30:36 -0300

    CONVERSOR: app operando correctamente

--- app/(conversor)/conversor.jsx
+++ app/(conversor)/conversor.jsx
@@ -1,15 +1,35 @@
 import { router } from 'expo-router';
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import styled from 'styled-components/native';
 
 export default function Conversor() {
+    const [cm, setCm] = useState('');
+    const [result, setResult] = useState(null);
+
+    const cmToM = () => {
+        if(!cm) {
+            Alert.alert('Error', 'Por favor, ingresa un valor en centímetros');
+            return;
+        }
+
+        const mts = parseFloat(cm)/100;
+        setResult(mts);
+    };
+
+    // onChangeText: capturará cada tipeo.
     return (<Container>
-        <Input placeholder='Enter your measurement number'></Input>
-        <CalculateButton>
+        <Input
+            placeholder='Enter your measurement number'
+            keyboardType="numeric"
+            onChangeText={setCm}
+        />
+        <CalculateButton onPress={cmToM}>
             <ButtonText>
                 Calcular
             </ButtonText>
         </CalculateButton>
-        <ResultText>Resultado: X</ResultText>
+        <ResultText>Resultado: {result!==null ? `${result} m` : '-'}</ResultText>
         <BackButton>
             <ButtonText onPress={() => router.back()}>
                 Go Back
