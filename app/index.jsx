8865829b4dbf068385e2128364518a9a3655d700
Nahuel-Ranz <elhacha_nahuel@hotmail.com>
2025-05-29 13:36:51 -0300

    enrutamiento por 'TouchableOpacity'. 'Platform': para saber de la plataforma. SafeAreaView.

--- app/index.jsx
+++ app/index.jsx
@@ -1,58 +1,80 @@
-import { FlatList, StatusBar } from 'react-native';
-import styled from 'styled-components/native';
-
-// uso de Link para el enrutamiento dinámico
-import { Link } from 'expo-router';
-
-// uso de iconos
 import { MaterialIcons } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { FlatList, Platform, StatusBar, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function Component() {
     
-    // Como segunda propiedad de cada objeto(grupo) le agregamos su ruta(absoluta desde 'app')
     const routes = [
         {name: 'conversor', ref: "/conversor"},
         {name: 'theory', ref: "/theory"}
     ];
 
-    // Implementación del Link. Requiere una prop. href.
     const renderItem = ({item}) => {
-        return (<LinkButton href={item.ref}>
-            <MaterialIcons name='insert-drive-file' size={24}/>
-            <Title>{item.name}</Title>
-        </LinkButton>);
+        return (<TouchableOpacity onPress={() => router.push(item.ref)}>
+            <LinkButton href={item.ref}>
+                <IconContainer>
+                    <MaterialIcons name='insert-drive-file' size={24} color='green'/>
+                    <Name>{item.name}</Name>
+                </IconContainer>
+                <Arrow name='arrow-right'/>
+            </LinkButton>
+        </TouchableOpacity>);
     }
 
-    return (<Container>
+    // Con el Platform nos aseguramos de que el dispositivo sobre el que se ejecuta nuestra aplicación será o no IOS.
+    return (<Container style={{ paddingTop: Platform.OS === 'ios' ? 0 : 50 }}>
         <StatusBar barStyle={'auto'}></StatusBar>
         <Title style={{marginTop:50}}>Menu</Title>
-        <FlatList
-            keyExtractor={(item) => item.name}   // "extrae" un 'id' del elemento si es que este no lo tiene.
-            data={routes}            // trabaja con el arreglo antes definido.
-            renderItem={renderItem}  // renderiza cada elemento de la lista con la que se trabaja, en este caso, el arreglo.
+        <List
+            keyExtractor={(item) => item.name}
+            data={routes}
+            renderItem={renderItem}
         />
     </Container>);
 }
 
-const Container = styled.View`
+// SafeAreaView: brinda valores definidos para dispositivos IOS.
+const Container = styled.SafeAreaView`
     flex: 1;
     justifyContent: center;
     alignItems: center;
     gap: 15px;
+    backgroundColor: black;
 `;
 
 const Title = styled.Text`
-
+    color: white;
+    fontSize: 30;
 `;
 
-// estilos para el renderItem.
-// como Link no es nativo, sino que viene de expo-router, lo indicamos entre paréntesis.
-const LinkButton = styled(Link)`
+const LinkButton = styled.View`
     border: 2px solid #333;
     border-radius: 20px;
     padding: 10px;
     marginBottom: 15;
     flex-direction: row;
+    justify-content: space-between;
     align-items: center;
+`;
+
+const Name = styled.Text`
+    color: lightgreen;
+    fontSize: 27;
+`;
+
+const Arrow = styled(MaterialIcons)`
+    color: green;
+    fontSize: 40;
+`;
+
+const List = styled(FlatList)`
     width: 100%;
+    padding: 10px;
+`;
+
+const IconContainer = styled.View`
+    gap: 10;
+    flex-direction: row;
+    align-items: center;
 `;
